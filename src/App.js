7cda5f896a6c08134c73f9cbba1532806a813efd
Arefin890 <ajraihan122@gmail.com>
2022-01-29 23:14:20 +0600

    added Order Review

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import './App.css';
+
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
